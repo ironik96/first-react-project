82e1082219e462737834089c40a38001884187dd
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-05-08 20:50:04 +0300

    Solve Warehouse Product List Task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
+import products from "./products";
+
+function Product(props) {
+  return (
+    <div className="product-list">
+      <span>
+        {" "}
+        <img className="product-img" src={props.image} alt="product" />{" "}
+      </span>
+      <span> {props.name} </span>
+      <span> {props.price} </span>
+    </div>
+  );
+}
 
 function App() {
+  const productsUI = products.map((p) => (
+    <Product key={p.id} name={p.name} price={p.price} image={p.image} />
+  ));
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ahmad Bookstore</h1>
         <p>Quality books that you will not find elsewhere (not even here :D)</p>
-        <img src={require("./empty-shelf.jpg")} alt="empty shelf" />
+        <img src={require("./images/empty-shelf.jpg")} alt="empty shelf" />
+        {productsUI}
       </header>
     </div>
   );
---
+++ src/products.js
@@ -0,0 +1,18 @@
+import image1 from "./images/book.png";
+
+const products = [
+  {
+    id: 1,
+    name: `product1`,
+    price: "1KD",
+    image: image1,
+  },
+  {
+    id: 2,
+    name: `product2`,
+    price: "1KD",
+    image: image1,
+  },
+];
+
+export default products;
